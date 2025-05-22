688c4c8620a23edc39005d1d467f386fe213fd31
Sofia Ramirez <nicoles.ramirezo@uqvirtual.edu.co>
2025-05-22 08:52:16 -0500

    update
    creacion completa de la clase padre libro y las 3 hijas con el enum tipoFormato

--- TrabajoFInalBiblioteca/src/main/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/LibroFisico.java
+++ TrabajoFInalBiblioteca/src/main/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/LibroFisico.java
@@ -1,4 +1,56 @@
 package co.edu.uniquindio.poo.trabajofinalbiblioteca.Model;
 
-public class LibroFisico {
+public class LibroFisico extends Libro {
+
+    private int numeroPaginas;
+    private String editorial;
+    private String ubicacion;
+
+    /**
+     * Metodo constructor de la clase hija LibroFisico
+     * @param nombre
+     * @param autor
+     * @param anioPublicacion
+     * @param disponibilidad
+     * @param numeroPaginas
+     * @param editorial
+     * @param ubicacion
+     */
+    public LibroFisico(String nombre, String autor, int anioPublicacion, boolean disponibilidad, int numeroPaginas, String editorial, String ubicacion) {
+        super(nombre, autor, anioPublicacion, disponibilidad);
+        this.numeroPaginas = numeroPaginas;
+        this.editorial = editorial;
+        this.ubicacion = ubicacion;
+
+    }
+
+    /**
+     * Metodos getter y setter de los atributos propios de la clase hija libro fisico
+     * @return
+     */
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+
 }
--- TrabajoFInalBiblioteca/src/main/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/LibroReferencia.java
+++ TrabajoFInalBiblioteca/src/main/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/LibroReferencia.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.poo.trabajofinalbiblioteca.Model;
 
-public class LibroReferencia {
+public class LibroReferencia extends Libro {
+
+    public LibroReferencia(String nombre, String autor, int anioPublicacion, boolean disponibilidad) {
+        super(nombre, autor, anioPublicacion, disponibilidad);
+
+
+        
+    }
 }
