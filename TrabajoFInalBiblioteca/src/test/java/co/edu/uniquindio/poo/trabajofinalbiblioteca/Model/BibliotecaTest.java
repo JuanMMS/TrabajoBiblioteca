24224558e6d3a05fe217e091556eb2e47741378e
JuanMMS <juanm.mosqueras@uqvirtual.edu.co>
2025-05-22 22:09:05 -0500

    Uptate

---
+++ TrabajoFInalBiblioteca/src/test/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/BibliotecaTest.java
@@ -0,0 +1,44 @@
+package co.edu.uniquindio.poo.trabajofinalbiblioteca.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BibliotecaTest {
+
+    @Test
+    void agregarEmpleadoTest() {
+        Biblioteca biblioteca = new Biblioteca("Los libres", "123A", "Cra 4, Mz 20");
+        Empleado empleado = new Bibliotecario("Juan José", "10987", "juanjo@gmail.com", "JuanJ22", "ABCD123");
+        biblioteca.agregarEmpleado(empleado);
+        assertTrue(biblioteca.getListEmpleados().contains(empleado));
+    }
+
+    @Test
+    void visualizarEmpleadoTest() {
+    }
+
+    @Test
+    void actualizarEmpleadoTest() {
+    }
+
+    @Test
+    void eliminarEmpleadoTest() {
+    }
+
+    @Test
+    void agregarLibroTest() {
+    }
+
+    @Test
+    void visualizarLibroTest() {
+    }
+
+    @Test
+    void actualizarLibroTest() {
+    }
+
+    @Test
+    void testEliminarEmpleadoTest() {
+    }
+}
