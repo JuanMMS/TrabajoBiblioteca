a9a13253cd429b54a23b7d4f005fe7eff9dab735
JuanMMS <juanm.mosqueras@uqvirtual.edu.co>
2025-05-26 23:12:00 -0500

    Update
    Se termina de hacer los test de bibliotecario

--- TrabajoFInalBiblioteca/src/test/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/BibliotecarioTest.java
+++ TrabajoFInalBiblioteca/src/test/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/BibliotecarioTest.java
@@ -179,30 +179,121 @@ class BibliotecarioTest {
     }
 
     @Test
-    void agregarLibro() {
+    void agregarLibroTest() {
+        // Crear la biblioteca, el bibliotecario y el libro
+        Biblioteca biblioteca1 = new Biblioteca("Casa", "1234", "Uniquindio");
+        Bibliotecario bibliotecario1 = new Bibliotecario("Pedro", "ABC", "Pedro123@uqvirtual.edu.co", "PedroM12", "12345");
+        LibroFisico libro1 = new LibroFisico("Don Quijote", "Cervantes", 2000, 5500, "Filmore", "L2B");
+
+        //Se agrega el libro a la biblioteca
+        bibliotecario1.agregarLibro(biblioteca1, libro1);
+
+        //verificación
+        assertTrue(biblioteca1.getListLibros().contains(libro1));
+
     }
 
     @Test
     void devolverLibro() {
+        // Crear la biblioteca y el bibliotecario
+        Biblioteca biblioteca1 = new Biblioteca("Casa", "1234", "Uniquindio");
+        Bibliotecario bibliotecario1 = new Bibliotecario("Pedro", "ABC", "Pedro123@uqvirtual.edu.co", "PedroM12", "12345");
+
+        // Crear usuario y libro
+        Persona usuario1 = new Estudiante("Juan", "111");
+
+        LibroFisico libro1 = new LibroFisico("Don Quijote", "Cervantes", 2000, 5500, "Filmore", "L2B");
+
+        // Registrar préstamo
+        Prestamo prestamo1 = new Prestamo("1", libro1, 5, usuario1, bibliotecario1);
+        //Se agrega los prestamos al bibliotecario
+        bibliotecario1.agregarPrestamo(biblioteca1, prestamo1);
+
+        //Se devuelve el libro prestado
+        bibliotecario1.devolverLibro(prestamo1);
+        //Tomamos el estado del prestamo
+        boolean libroDevuelto = prestamo1.isDevuelto();
+
+        assertTrue(libroDevuelto);
+
     }
 
     @Test
     void agregarPersona() {
+        // Crear la biblioteca y el bibliotecario
+        Biblioteca biblioteca1 = new Biblioteca("Casa", "1234", "Uniquindio");
+        Bibliotecario bibliotecario1 = new Bibliotecario("Pedro", "ABC", "Pedro123@uqvirtual.edu.co", "PedroM12", "12345");
+        // Crear usuario
+        Persona usuario1 = new Estudiante("Juan", "111");
+
+        //Agregar persona a biblioteca mediante bibliotecario
+        bibliotecario1.agregarPersona(biblioteca1, usuario1);
+        //verificación
+        assertTrue(biblioteca1.getListPersonas().contains(usuario1));
+
     }
 
     @Test
     void visualizarPersona() {
+        // Crear la biblioteca y el bibliotecario
+        Biblioteca biblioteca1 = new Biblioteca("Casa", "1234", "Uniquindio");
+        Bibliotecario bibliotecario1 = new Bibliotecario("Pedro", "ABC", "Pedro123@uqvirtual.edu.co", "PedroM12", "12345");
+        // Crear usuario
+        Persona usuario1 = new Estudiante("Juan", "111");
+
+        //Agregar persona a biblioteca mediante bibliotecario
+        bibliotecario1.agregarPersona(biblioteca1, usuario1);
+
+        //Visualizar
+        String esperado = usuario1.toString();
+        String resultado = bibliotecario1.visualizarPersona(biblioteca1, usuario1);
+
+        assertEquals(esperado, resultado);
     }
 
     @Test
-    void actualizarPersona() {
+    void actualizarPersonaTest() {
+        //Creamos biblioteca, persona y bibliotecario
+        Biblioteca biblioteca = new Biblioteca("Los libros", "444", "cr19N");
+        Persona persona = new Persona("Carlos", "300");
+        Bibliotecario bibliotecario = new Bibliotecario("Admin", "001", "admin@uq.edu.co", "admin", "admin123");
+        //Actualizamos la información de una persona
+        bibliotecario.actualizarPersona(biblioteca, persona ,"Carlos Pérez", "999");
+
+        //Verificaciones
+        assertEquals("Carlos Pérez", persona.getNombre());
+        assertEquals("999", persona.getId());
     }
 
     @Test
     void eliminarPersona() {
+        //Creamos biblioteca, persona y bibliotecario
+        Biblioteca biblioteca = new Biblioteca("Los libros", "444", "cr19N");
+        Persona persona = new Persona("Carlos", "300");
+        Bibliotecario bibliotecario = new Bibliotecario("Admin", "001", "admin@uq.edu.co", "admin", "admin123");
+        //Actualizamos la información de una persona
+        bibliotecario.actualizarPersona(biblioteca, persona ,"Carlos Pérez", "999");
+        //Verificaciones
+        bibliotecario.eliminarPersona(biblioteca, persona);
+        assertFalse(biblioteca.getListPersonas().contains(persona));
     }
 
     @Test
     void agregarPrestamo() {
+        // Crear la biblioteca y el bibliotecario
+        Biblioteca biblioteca1 = new Biblioteca("Casa", "1234", "Uniquindio");
+        Bibliotecario bibliotecario1 = new Bibliotecario("Pedro", "ABC", "Pedro123@uqvirtual.edu.co", "PedroM12", "12345");
+        // Crear usuarios y libros
+        Persona usuario1 = new Estudiante("Juan", "111");
+        LibroFisico libro1 = new LibroFisico("Don Quijote", "Cervantes", 2000, 5500, "Filmore", "L2B");
+        // Registrar préstamos
+        Prestamo prestamo1 = new Prestamo("1", libro1, 5, usuario1, bibliotecario1);
+
+        //Se agrega los prestamos al bibliotecario
+        bibliotecario1.agregarPrestamo(biblioteca1, prestamo1);
+
+        //verificacion
+        assertTrue(biblioteca1.getListPrestamos().contains(prestamo1));
+
     }
 }
