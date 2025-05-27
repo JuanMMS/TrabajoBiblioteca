bcbdf89c283bf14ad9ba69c032f8e326aa7a0ed3
JuanMMS <juanm.mosqueras@uqvirtual.edu.co>
2025-05-26 22:02:18 -0500

    Big update
    Se arregla clase bibliotecario y queda full hd

--- TrabajoFInalBiblioteca/src/test/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/BibliotecarioTest.java
+++ TrabajoFInalBiblioteca/src/test/java/co/edu/uniquindio/poo/trabajofinalbiblioteca/Model/BibliotecarioTest.java
@@ -103,4 +103,38 @@ class BibliotecarioTest {
 
 
     }
+
+    @Test
+    void libroMasPrestadoTest() {
+        // Crear la biblioteca y el bibliotecario
+        Biblioteca biblioteca1 = new Biblioteca("Casa", "1234", "Uniquindio");
+        Bibliotecario bibliotecario1 = new Bibliotecario("Pedro", "ABC", "Pedro123@uqvirtual.edu.co", "PedroM12", "12345");
+
+        // Crear usuarios y libros
+        Persona usuario1 = new Estudiante("Juan", "111");
+        Persona usuario2 = new Docente("Raul", "222");
+        Persona usuario3 = new Docente("Cris", "333");
+        LibroFisico libro1 = new LibroFisico("Don Quijote", "Cervantes", 2000, 5500, "Filmore", "L2B");
+        LibroFisico libro2 = new LibroFisico("1984", "Orwell", 1985, 422, "Filmore", "L3B");
+        LibroFisico libro3 = new LibroFisico("21 minutos", "Juan Carlos Bodoque", 2017, 400, "21Min", "L3B");
+
+        // Registrar préstamos
+        Prestamo prestamo1 = new Prestamo("1", libro1, 5, usuario1, bibliotecario1);
+        Prestamo prestamo2 = new Prestamo("2", libro2, 2, usuario1, bibliotecario1);
+        Prestamo prestamo3 = new Prestamo("3", libro1, 3, usuario1, bibliotecario1); // mismo libro otra vez
+        Prestamo prestamo4 = new Prestamo("4", libro3, 2, usuario3, bibliotecario1);
+
+        //Se agrega los prestamos al bibliotecario
+        bibliotecario1.agregarPrestamo(biblioteca1, prestamo1);
+        bibliotecario1.agregarPrestamo(biblioteca1, prestamo2);
+        bibliotecario1.agregarPrestamo(biblioteca1, prestamo3);
+        bibliotecario1.agregarPrestamo(biblioteca1, prestamo4);
+
+        System.out.println(bibliotecario1.libroMasPrestado());
+
+        assertTrue(bibliotecario1.libroMasPrestado().contains("Don Quijote"));
+
+
+
+    }
 }
